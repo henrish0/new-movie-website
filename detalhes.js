a3d028f53e9f4618fa0bcd0a7989e7de2002bb11
Henrique Almeida <hcarvalhoalmeida7@gmail.com>
2022-06-21 19:49:42 -0300

    Create detalhes.js

---
+++ detalhes.js
@@ -0,0 +1,25 @@
+
+const params = new URLSearchParams(location.search);
+let id = params.get('id');
+
+dadosFilmes = JSON.parse(localStorage.getItem('db_filmes'));
+let idxFilme = dadosFilmes.results.findIndex((elem) => elem.id == id)
+
+if (idxFilme > -1) {
+    let filme = dadosFilmes.results[idxFilme];
+
+    document.getElementById('divDetalheFilme').innerHTML = `
+            <img class="imgcenter" src="https://image.tmdb.org/t/p/w500${filme.poster_path}" alt="Card image cap">
+            <div class="row">
+                <h1 style="font-size: 3rem;" class="text-center pt-5 pb-3">${filme.title}</h1>
+                <p style="font-size: 1.2rem;" class="cmmtxt text-center">${filme.overview}</p>
+            </div>
+         </div>    
+    
+         `
+}
+else {
+    alert('Filme não encontrado')
+}
+
+
